a9a1050ad8cb68894d2070abfe53cb391a5392bd
Chris <chrisnicole1104@gmail.com>
2025-04-19 13:20:49 +0200

    Aggiunte alcune di altre funzionalità, non interruzione del programma in caso di errore di input, aggiunta di diverse operazioni

--- ssgs-calc/calc.js
+++ ssgs-calc/calc.js
@@ -1,7 +1,9 @@
 const { resolve } = require('path');
 const op = require('./operazioni');
-//definizione di interfacci per gestire I/O
+//definizione di interfaccia per gestire I/O
 const readLine = require('readline');
+const { Console } = require('console');
+
 const rl =  readLine.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -16,9 +18,66 @@ function richiestaInput(richiesta){
 
 
 async function main(){
-    const a = parseFloat(await richiestaInput("Inserisci primo numero: "));
-    const b = 4;
-    console.log(op.somma(a,b));
+    console.log("Benvenuto, questa è la calcolatrice sviluppata in NodeJs richiesta -- CalcJs");
+    let a, b;
+    while(isNaN(a) || isNaN(b)){    
+        a = parseFloat(await richiestaInput("Inserisci primo numero: "));
+        b = parseFloat(await richiestaInput("Inserisci secondo numero: "));
+    }
+    /* per interrompere il programma e non farlo proseguire
+    if(isNaN(a)){
+        console.error( "Input invalido");
+        rl.close();
+        process.exit(1);
+    }
+   */
+    let operazione;
+    do{
+        console.log(" ------------ MENU ------------");
+        console.log("   1. Somma");
+        console.log("   2. Differenza");
+        console.log("   3. Divisione");
+        console.log("   4. Moltiplicazione");
+        console.log("   5. Potenza");
+        console.log("   6. Modulo");
+        console.log("   7. Fattoriale");
+        console.log("   0. Fine");
+        operazione = parseInt(await richiestaInput("Inserisci l'operazione che desideri effettuare: "));
+        if(isNaN(operazione) || operazione < 0 || operazione > 7){
+            console.error( "Scelta invalida");
+            rl.close();
+            process.exit(1);
+        }
+        switch(operazione){
+            case 1:
+                console.log(op.somma(a,b));
+                break;
+            case 2:
+                console.log(op.differenza(a,b));
+                break;
+            case 3:
+                console.log(op.divisione(a,b));
+                break;
+            case 4:
+                console.log(op.moltiplicazione(a,b));
+                break;
+            case 5:
+                console.log(op.pow(a,b));
+                break;
+            case 6:
+                a = op.swapsign(a);
+                b = op.swapsign(b);
+                console.log(a,b);
+                break;
+            case 7: 
+                console.log(op.fact(a));
+                break;
+            default:
+                break;
+
+        }
+    }while(operazione != 0);
+    console.log("Grazie per aver usato la calcolatrice, a presto!");
     rl.close();
 }
 
