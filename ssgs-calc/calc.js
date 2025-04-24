e1e30ca20bebdc436ef8b83c0a650ae08f8a8aaf
Chris <chrisnicole1104@gmail.com>
2025-04-24 23:05:00 +0200

    Ultime modifiche riguardanti ripetizione di righe di codice

--- ssgs-calc/calc.js
+++ ssgs-calc/calc.js
@@ -20,19 +20,7 @@ function richiestaInput(richiesta){
 async function main(){
     
     console.log("  Benvenuto, questa è la calcolatrice semplice sviluppata in NodeJs richiesta -- CalcJs (a soli due numeri) ");
-    let a, b;
-    while(isNaN(a) || isNaN(b)){    
-        a = parseFloat(await richiestaInput("Inserisci primo numero: "));
-        b = parseFloat(await richiestaInput("Inserisci secondo numero: "));
-    }
-    /* per interrompere il programma e non farlo proseguire
-    if(isNaN(a)){
-        console.error( "Input invalido");
-        rl.close();
-        process.exit(1);
-    }
-   */
-    let operazione;
+    let a = 0, b = 0, operazione;
     do{
         console.log(" ------------ MENU ------------");
         console.log("   1. Somma");
@@ -41,39 +29,47 @@ async function main(){
         console.log("   4. Moltiplicazione");
         console.log("   5. Potenza");
         console.log("   6. Modulo");
-        console.log("   7. Inserimento dei due numeri");
+        console.log("   7. Fattoriale");
+        console.log("   8. Cambio segno");
         console.log("   0. Fine");
         operazione = parseInt(await richiestaInput("Inserisci l'operazione che desideri effettuare: "));
-        while(isNaN(operazione) || operazione < 0 || operazione > 7){
+        while(isNaN(operazione) || operazione < 0 || operazione > 8){
             operazione = parseInt(await richiestaInput("Inserisci l'operazione che desideri effettuare: "));
         }
         switch(operazione){
             case 1:
+                ({a,b}= await opBinary());
                 console.log("Somma pari a: ", op.somma(a,b));
                 break;
             case 2:
+                ({a,b}= await opBinary());
                 console.log("Differenza pari a: ",op.differenza(a,b));
                 break;
             case 3:
+                ({a,b}= await opBinary());
                 if(op.divisione(a,b) == null) console.error('Impossibile effettuare divisione con denominatore = 0');
                 else console.log("Divisione pari a: ",op.divisione(a,b));
                 break;
             case 4:
+                ({a,b}= await opBinary());
                 console.log("Moltiplicazione pari a: ",op.moltiplicazione(a,b));
                 break;
             case 5:
+                ({a,b}= await opBinary());
                 console.log("Potenza pari a: ",op.pow(a,b));
                 break;
             case 6:
+                a = await opUnary();;
                 a = op.sign(a);
-                b = op.sign(b);
-                console.log("Moduli dei due numeri pari a: ",a,b);
+                console.log("Moduli dei due numeri pari a: ",a);
                 break;
             case 7:
-                do{    
-                    a = parseFloat(await richiestaInput("Inserisci primo numero: "));
-                    b = parseFloat(await richiestaInput("Inserisci secondo numero: "));
-                }while(isNaN(a) || isNaN(b))
+                a = await opUnary();
+                console.log("Moduli dei due numeri pari a: ",op.fact(a));
+                break;
+            case 8:
+                a = await opUnary();
+                console.log("Moduli dei due numeri pari a: ",op.sign(a));
                 break;
             default:
                 break;
@@ -83,5 +79,20 @@ async function main(){
     console.log("Grazie per aver usato CalcJS, a presto!");
     rl.close();
 }
-
+// to do mettere apposto le ultime due funzioni e fare push
+async function opBinary(){
+    let a,b;
+    do{    
+        a = parseFloat(await richiestaInput("Inserisci primo numero: "));
+        b = parseFloat(await richiestaInput("Inserisci secondo numero: "));
+    }while(isNaN(a) || isNaN(b))
+    return {a,b};
+}
+async function opUnary(){
+    let a;
+    do{
+        a = parseFloat(await richiestaInput("Inserisci il numero: "));
+    }while(isNaN(a));
+    return a;
+}
 main();
