174f1d40dc19483546ab8f87277b67b8fdb55654
ChrisHerrera03 <149780014+ChrisHerrera03@users.noreply.github.com>
2025-04-27 11:08:56 +0200

    Update somma.test.js

--- ssgs-calc/tests/somma.test.js
+++ ssgs-calc/tests/somma.test.js
@@ -2,7 +2,7 @@ const op = require('../operazioni');
 test('somma tra 2 e 2 è uguale a 4', () => {
     expect(op.somma(2,2)).toBe(4);
 });
-test('somma tra 3 e 5 è uguale a 7', () => {
+test('somma tra 3 e 5 è uguale a 8', () => {
     expect(op.somma(3,5)).toBe(8);
 })
 
